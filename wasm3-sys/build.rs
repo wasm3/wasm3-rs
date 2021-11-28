23b41109f495cb7e9423c510ee1533ec9dff9b89
Jernej Kos <jernej@kos.mx>
2021-11-28 13:05:20 +0100

    wasm3-sys: Cleanup

--- wasm3-sys/build.rs
+++ wasm3-sys/build.rs
@@ -1,8 +1,4 @@
-use std::env;
-use std::ffi::OsStr;
-use std::fmt::Write as _;
-use std::fs;
-use std::path::{Path, PathBuf};
+use std::{env, ffi::OsStr, fs, path::PathBuf};
 
 static WASM3_SOURCE: &str = "wasm3/source";
 const WHITELIST_REGEX_FUNCTION: &str = "([A-Z]|m3_).*";
@@ -12,36 +8,12 @@ const PRIMITIVES: &[&str] = &[
     "f64", "f32", "u64", "i64", "u32", "i32", "u16", "i16", "u8", "i8",
 ];
 
-fn gen_wrapper(out_path: &Path) -> PathBuf {
-    // TODO: we currently need the field definitions of the structs of wasm3. These aren't exposed
-    // in the wasm3.h header so we have to generate bindings for more.
-    let wrapper_file = out_path.join("wrapper.h");
-
-    let mut buffer = String::new();
-    fs::read_dir(WASM3_SOURCE)
-        .unwrap_or_else(|_| panic!("failed to read {} directory", WASM3_SOURCE))
-        .filter_map(Result::ok)
-        .map(|entry| entry.path())
-        .filter(|path| path.extension().and_then(OsStr::to_str) == Some("h"))
-        .for_each(|path| {
-            writeln!(
-                &mut buffer,
-                "#include \"{}\"",
-                path.file_name().unwrap().to_str().unwrap()
-            )
-            .unwrap()
-        });
-    fs::write(&wrapper_file, buffer).expect("failed to create wasm3 wrapper file");
-    wrapper_file
-}
-
 fn gen_bindings() {
     let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
 
-    let wrapper_file = gen_wrapper(&out_path);
-
     let mut bindgen = bindgen::builder()
-        .header(wrapper_file.to_str().unwrap())
+        .header("wasm3/source/wasm3.h")
+        .header("wasm3/source/m3_env.h")
         .use_core()
         .ctypes_prefix("cty")
         .layout_tests(false)
@@ -69,13 +41,8 @@ fn gen_bindings() {
                 ),
                 &format!(
                     "-Dd_m3HasFloat={}",
-                    if cfg!(feature = "floats") {
-                        1
-                    } else {
-                        0
-                    }
+                    if cfg!(feature = "floats") { 1 } else { 0 }
                 ),
-                "-Dd_m3LogOutput=0",
                 "-Dd_m3VerboseErrorMessages=0",
                 "-Iwasm3/source",
             ]
@@ -101,7 +68,6 @@ fn main() {
     );
 
     cfg.cpp(false)
-        .define("d_m3LogOutput", Some("0"))
         .warnings(false)
         .extra_warnings(false)
         .include(WASM3_SOURCE);
@@ -128,10 +94,6 @@ fn main() {
         } else {
             cfg.flag(&extra_clang_args);
         };
-    }
-
-    if cfg!(feature = "wasi") {
-        cfg.define("d_m3HasWASI", None);
     }
 
     cfg.define(
